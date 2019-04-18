17cc2a51f3776b51a1ddd30352a0817ee1d593a0
Hermes Colon <hermes.colon@upr.edu>
2019-04-18 12:21:18 -0400

    WinState Mario

---
+++ src/Game/GameStates/MarioWinState.java
@@ -0,0 +1,57 @@
+package Game.GameStates;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+import Display.UI.UIManager;
+import Display.UI.UIStringButton;
+import Main.Handler;
+import Resources.Images;
+
+/**
+ * Created by AlexVR on 7/1/2018.
+ */
+public class MarioWinState extends State {
+
+	public MarioWinState(Handler handler) {
+		super(handler);
+		uiManager = new UIManager(handler);
+		handler.getMouseManager().setUimanager(uiManager);
+
+		uiManager.addObjects(new UIStringButton(56, 100, 128, 64, "Retry", () -> {
+			handler.getMouseManager().setUimanager(null);
+			handler.getGame().reStart();
+			State.setState(handler.getGame().gameState);
+		},handler,Color.YELLOW));
+
+		uiManager.addObjects(new UIStringButton(56, 100+(64), 128, 64, "Options", () -> {
+			handler.getMouseManager().setUimanager(null);
+			handler.setIsInMap(false);
+			State.setState(handler.getGame().menuState);
+		},handler,Color.YELLOW));
+
+		uiManager.addObjects(new UIStringButton(56, 100+(128), 128, 64, "Title", () -> {
+			handler.getMouseManager().setUimanager(null);
+			handler.setIsInMap(false);
+			State.setState(handler.getGame().menuState);
+		},handler,Color.YELLOW));
+	}
+
+	private UIManager uiManager;
+
+	
+
+	@Override
+	public void tick() {
+		handler.getMouseManager().setUimanager(uiManager);
+		uiManager.tick();
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawImage(Images.marioWins,0,0,handler.getWidth(),handler.getHeight(),null);
+		uiManager.Render(g);
+	}
+
+}
